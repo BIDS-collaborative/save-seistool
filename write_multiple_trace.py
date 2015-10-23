f601f8bd1e5755dfe212642664d21a3e3f0cd967
ProximaDas <prox.me@gmail.com>
2015-10-23 10:10:04 -0700

    Tweaks

--- write_multiple_trace.py
+++ write_multiple_trace.py
@@ -4,9 +4,12 @@
 # Imports
 import glob
 from obspy.core import read
+from obspy.core.stream import Stream
+from obspy.core.trace import Trace
 
 # Read multiple mSEED files
-st = []
+# st = read("/Users/Proxima/Documents/classes/Fall2015/hacking-measurement/save-seistool/examples/CHILE_M8.3/BDM.BK.LHN.00.D.2015.259.225431.gz")
+st = Stream()
 i = 0
 
 # glob.glob returns a list of files in the directory
@@ -15,11 +18,15 @@ for file in glob.glob("/Users/Proxima/Documents/classes/Fall2015/hacking-measure
 	if (i>5):
 		break
 	else:
- 		st.append(read(file))
+ 		st += read(file)
 
+st.write("5_trace.mseed",format="mSEED")
+# for stream in st:
+# 	# stream.write("5_trace.mseed",format="mSEED")
+# 	multiplestream += stream
 
-for stream in st:
-	stream.write("5_trace.mseed",format="mSEED")
+# multiplestream = Stream(traces=[Trace(s) for s in st])
+# multiplestream.write("5_trace_test.mseed",format="MSEED")
 
 print("5 streams written into file 5_trace.mseed")
 	
